0c2a412c00a392ee77fb49e329ad38a001c7aee1
Chris Stewart <mils8545@gmail.com>
2021-12-24 12:02:47 -0700

    Day 22 - Started Cube Comparison

--- 22/app.js
+++ 22/app.js
@@ -7,8 +7,27 @@ const readFile = (readFile) => {
   return lines;
 };
 
+const checkOverlap = (box1, box2) => {
+  let overlap = false;
+  if ((((box1.x1 >= box2.x1) && (box1.x1 <= box2.x2)) || ((box2.x1 >= box1.x1) && (box2.x1 <= box1.x2))) 
+    && (((box1.y1 >= box2.y1) && (box1.y1 <= box2.y2)) || ((box2.y1 >= box1.y1) && (box2.y1 <= box1.y2))) 
+    && (((box1.z1 >= box2.z1) && (box1.z1 <= box2.z2)) || ((box2.z1 >= box1.z1) && (box2.z1 <= box1.z2)))) overlap = true;
+  return overlap;
+};
+
 const ex1 = (file) => {
   const lines = readFile(file);
+
+  let boxes = lines.map(line => {
+    const xyz = line.replaceAll("=", ',').replaceAll("..", ",").split(",");
+    const on = (line[1] == 'f') ? false : true;
+    return {x1: Number(xyz[1]), x2: Number(xyz[2]), y1: Number(xyz[4]), y2: Number(xyz[5]), z1: Number(xyz[7]), z2: Number(xyz[8]), on: on };
+  });
+
+  console.log(checkOverlap(boxes[18], boxes[15]));
+
+
+  console.log(boxes[4]);
 
 //  console.log(`EX 22-1: The losers score times the number of rolls is ${loserRolls}.`);
 };
